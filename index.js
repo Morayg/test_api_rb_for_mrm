6f84ed3dfcf9dfde5af69074429ba37d8362e3af
morayg <moraygg@gmail.com>
2018-04-17 17:17:37 +0300

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var token_expiration_timestamp = new Date('2018-05-15').getTime();
 var date_now =  new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 var secret;
 try {  
-    secret = fs.readFileSync('key.txt', 'utf8');
+    secret = fs.readFileSync('./key.txt', 'utf8');
     //console.log(secret);    
 } catch(e) {
     console.log('Error:', e.stack);
@@ -19,21 +19,18 @@ var token = jwt.sign({
     iss: 'auth.sberbank-school.ru',
     aud: ['default', 'auth', 'default'],
     validTill: token_expiration_timestamp,
-    tableNum: '00000000',
+    tableNum: '00000013',
     role: 'rb'
 }, secret, {algorithm: 'HS512'});
 
 //Указываем ENDPOINT, и параметры необходимые для метода
 var options = {
-    url: 'https://api.stage.sberbank-school.ru/v2/mrm/mobile/log/videonews/view',
+    url: 'https://api.stage.sberbank-school.ru/v2/mrm/mobile/videos/video/setCompleted',
     headers: {Authorization: 'Bearer ' + token},
     preambleCRLF: true,
     postambleCRLF: true,
     json: {
-	startEventDatetime: date_now,
-	endEventDatetime: date_now,
-	tableNum: 'Не используется',
-	eventObjectId: 1
+	videoId: 1
     }
 };
 
